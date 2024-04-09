f535ff42ce41d9fb5bb50d8a841ab773308778e4
Fan Huang <fhuang@ritchiebros.com>
2024-04-09 20:43:08 +0800

    add file

---
+++ src/main/kotlin/com/fan/db/entity/SearchLog.kt
@@ -0,0 +1,23 @@
+package com.fan.db.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "search_log")
+class SearchLog(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long? = null,
+    @Column(name = "count", nullable = false)
+    val count: Int,
+    @Column(name = "date", nullable = false)
+    var date: String,
+    @Column(name = "keyword", nullable = false)
+    var keyword: String,
+
+    ) {
+
+}
