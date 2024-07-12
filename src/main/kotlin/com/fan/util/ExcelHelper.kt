9d846b7ddf0260848d03678695c1a729fe726a05
Fan Huang <fanhuang@melco-resorts.com>
2024-07-12 16:50:05 +0800

    fix download error

--- src/main/kotlin/com/fan/util/ExcelHelper.kt
+++ src/main/kotlin/com/fan/util/ExcelHelper.kt
@@ -3,31 +3,51 @@ package com.fan.util
 import cn.hutool.poi.excel.ExcelUtil
 import cn.hutool.poi.excel.ExcelWriter
 import jakarta.servlet.http.HttpServletResponse
+import org.apache.poi.ss.usermodel.Sheet
 import org.springframework.http.ContentDisposition
 import org.springframework.http.HttpHeaders
 import java.nio.charset.StandardCharsets
 
 object ExcelHelper {
-    fun writeRows(headers: List<String>, contents: List<List<String>>): ExcelWriter {
+    fun writeRows(
+        headers: List<String>,
+        contents: List<List<String>>,
+    ): ExcelWriter {
         val rows = listOf(headers, *contents.toTypedArray())
         val writer = ExcelUtil.getWriter(true)
         writer.write(rows, true)
         headers.forEachIndexed { index, _ ->
             val sheet = writer.sheet
             sheet.autoSizeColumn(index)
-            sheet.setColumnWidth(index, sheet.getColumnWidth(index) * 15 / 10);
+            sheet.setColumnWidth(index, getMaxWidth(sheet, index))
         }
 
         return writer
     }
 
-    fun flushToResponse(httpServletResponse: HttpServletResponse, writer: ExcelWriter, fileName: String) {
+    private fun getMaxWidth(
+        sheet: Sheet,
+        index: Int,
+    ): Int {
+        val target = sheet.getColumnWidth(index) * 15 / 10
+        return if (target > 255) 255 else target
+    }
+
+    fun flushToResponse(
+        httpServletResponse: HttpServletResponse,
+        writer: ExcelWriter,
+        fileName: String,
+    ) {
         httpServletResponse.characterEncoding = "UTF-8"
         httpServletResponse.contentType =
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
         httpServletResponse.setHeader(
             HttpHeaders.CONTENT_DISPOSITION,
-            ContentDisposition.attachment().filename(fileName, StandardCharsets.UTF_8).build().toString()
+            ContentDisposition
+                .attachment()
+                .filename(fileName, StandardCharsets.UTF_8)
+                .build()
+                .toString(),
         )
         httpServletResponse.outputStream.use {
             writer.flush(it)
